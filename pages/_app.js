6087adcbd8694ed3e130868bb31f7c5aaeb658e7
Diego F. Ticona Ramos <diegonano09@gmail.com>
2022-09-08 10:03:34 -0400

    fix: :zap: estilos one-dark para codigo en posts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
-import "../styles/prism-night-owl.css";
+// import "../styles/prism-night-owl.css";
+import "../styles/prism-one-dark.css";
+// import "../styles/prism-one-light.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
